419d22b0575b26fd5f2296b82298f0e0b853efd0
designer (Designer) <no-reply@simplicite.fr>
2019-03-11 22:17:43 +0100

    Updated

---
+++ src/com/simplicite/objects/Demo/DemoSupplier.java
@@ -0,0 +1,14 @@
+package com.simplicite.objects.Demo;
+
+import java.util.*;
+import com.simplicite.util.*;
+import com.simplicite.util.tools.*;
+
+/**
+ * Business object DemoSupplier
+ */
+public class DemoSupplier extends ObjectDB {
+	private static final long serialVersionUID = 1L;
+	
+	
+}
