d04d289c76efdc2355a7f6084fe421d037075623
David AZOULAY <dazoulay@simplicite.fr>
2019-03-25 12:26:25 +0100

    Moved test

--- test/src/test/DemoProductTest.java
+++ test/src/test/DemoProductTest.java
@@ -2,13 +2,16 @@ package com.simplicite.test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
 import com.simplicite.objects.Demo.DemoProduct;
 
 /**
  * Local unit test example
  */
 public class DemoProductTest {
-	/** Increment test*/
+	/** Increment test */
+	@Test
 	public void testIncrement() {
 		assertEquals(10, DemoProduct.INCREMENT);
 	}
