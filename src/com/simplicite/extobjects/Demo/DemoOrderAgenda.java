0c5ae5eb74097a081e2d91b3094ddfb19e92059e
designer (Designer) <no-reply@simplicite.fr>
2019-10-17 23:46:31 +0200

    Responsive only

--- src/com/simplicite/extobjects/Demo/DemoOrderAgenda.java
+++ src/com/simplicite/extobjects/Demo/DemoOrderAgenda.java
@@ -1,35 +1,15 @@
 package com.simplicite.extobjects.Demo;
 
-import com.simplicite.util.ExternalObject;
-import com.simplicite.util.tools.HTMLTool;
 import com.simplicite.util.tools.Parameters;
-import com.simplicite.webapp.Navigator;
 
 /**
  * Order agenda custom UI component
  */
-public class DemoOrderAgenda extends ExternalObject {
+public class DemoOrderAgenda extends com.simplicite.webapp.web.ResponsiveExternalObject {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * Display method
-	 * @param params Request parameters
-	 */
+	
 	@Override
-	public Object display(Parameters params) {
-		if (!getGrant().isResponsive()) { // Legacy version
-			boolean embedded = params.getBooleanParameter("embedded");
-			if (!embedded) {
-				appendCSSIncludes(HTMLTool.fullcalendarCSS());
-				appendJSIncludes(HTMLTool.fullcalendarJS(getGrant().getLang()));
-			}
-			String js = getName() + ".render(\"" + HTMLTool.getFormURL("DemoOrder", "agenda_DemoOrder", "ROWID", "nav="+Navigator.ADD) + "\");";
-			return (embedded
-					? HTMLTool.cssIncludes(HTMLTool.fullcalendarCSS()) +
-					  HTMLTool.jsIncludes(HTMLTool.fullcalendarJS(getGrant().getLang()))
-					: "") + HTMLTool.jsBlock(embedded ? js : HTMLTool.jsOnload(js));
-		} else { // Responsive version
-			return javascript("$ui.loadCalendar(function() { " + getName() + ".render(); });");
-		}
+	public String getRenderStatement(Parameters params) {
+		return "$ui.loadCalendar(function() { " + super.getRenderStatement(params) + " });";
 	}
 }
--- src/com/simplicite/extobjects/Demo/DemoPlaceNewOrder.java
+++ src/com/simplicite/extobjects/Demo/DemoPlaceNewOrder.java
@@ -1,20 +1,8 @@
 package com.simplicite.extobjects.Demo;
 
-import com.simplicite.util.ExternalObject;
-import com.simplicite.util.tools.Parameters;
-
 /**
  * Place new order custom UI component
  */
-public class DemoPlaceNewOrder extends ExternalObject {
+public class DemoPlaceNewOrder extends com.simplicite.webapp.web.ResponsiveExternalObject {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * Display method
-	 * @param params Request parameters
-	 */
-	@Override
-	public Object display(Parameters params) {
-		return javascript(getName() + ".render();");
-	}
 }
