2695bbef13139ccced0539f98f6f8506d0c34300
designer (Designer) <no-reply@simplicite.fr>
2019-12-18 16:17:46 +0100

    Updated

---
+++ test/src/com/simplicite/tests/Demo/DemoTests.java
@@ -0,0 +1,34 @@
+package com.simplicite.tests.Demo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import com.simplicite.objects.Demo.DemoProduct;
+import com.simplicite.util.Grant;
+import com.simplicite.util.ObjectField;
+
+/**
+ * Demo unit tests
+ */
+public class DemoTests {
+	/** Increment test */
+	@Test
+	public void testIncrement() {
+		try {
+			DemoProduct prd = (DemoProduct)Grant.getSystemAdmin().getTmpObject("DemoProduct");
+			prd.setValues(prd.search().get(0), true);
+			ObjectField s = prd.getField("demoPrdStock");
+			int n = s.getInt(0);
+			HashMap<String, String> params = new HashMap<>();
+			params.put("demoPrdIncrement", String.valueOf(DemoProduct.DEFAULT_INCREMENT));
+			prd.increaseStock(params);
+			assertEquals(n + DemoProduct.DEFAULT_INCREMENT, s.getInt(0));
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
