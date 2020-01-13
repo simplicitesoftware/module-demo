9c95028e722a52660e95269d93c4e1dc8765ba55
designer (Designer) <no-reply@simplicite.fr>
2020-01-13 13:34:04 +0100

    Map

--- resources/ObjectInternal/DemoClient/SCRIPT.js
+++ resources/ObjectInternal/DemoClient/SCRIPT.js
@@ -2,10 +2,8 @@
 // Client-side logic for customer business object
 //----------------------------------------------------
 var DemoClient = typeof DemoClient !== "undefined" ? DemoClient : (function($) {
-	var resp = typeof $ui !== "undefined";
-
-	// Google Map action
-	var map = function() {
+	// Display Google map
+	var _map = function() {
 		var rowId = getFieldValue("row_id");
 		if (rowId && rowId != "0") {
 			var n = $.escapeHTML(getFieldValue("demoCliFirstname") + " " + getFieldValue("demoCliLastname"));
@@ -21,47 +19,52 @@ var DemoClient = typeof DemoClient !== "undefined" ? DemoClient : (function($) {
 		}
 	};
 
-	// Google Hangouts button
-	var hangout = function() {
+	// Display Google Hangouts button
+	var _hangout = function() {
 		var email = getFieldValue("demoCliEmail");
 		var invites = email ? [{ id: email, invite_type: "EMAIL" }]: [];
 		gapi.hangout.render("hangout-button", { "render": "createhangout", "invites": invites });
 	};
 
-	if (resp) {
-		// Responsive UI hook
-		Simplicite.UI.hooks.DemoClient = function(o, cbk) {
-			// Minimalistic backward compatibility with legacy UI getFieldValue for responsive UI
-			window.getFieldValue = function(name) {
-				return $ui.getUIField(null, o, name).ui.val();
-			};
+	// Responsive UI hook
+	Simplicite.UI.hooks.DemoClient = function(o, cbk) {
+		// Minimalistic backward compatibility with legacy UI getFieldValue for responsive UI
+		window.getFieldValue = function(name) { return $ui.getUIField(null, o, name).ui.val(); };
 
-			try {
-				o.locals.ui.form.onload = function() {
-					try {
-						if (typeof gapi === "undefined" || typeof gapi.hangout === "undefined") {
-							$ui.loadScript({
-								url: "https://apis.google.com/js/platform.js",
-								onload: hangout
-							})
-						} else {
-							hangout();
-						}
-					} catch(el) {
-						console.error(el);
+		try {
+			// Display Hangout buttons on form load
+			o.locals.ui.form.onload = function() {
+				try {
+					if (typeof gapi === "undefined" || typeof gapi.hangout === "undefined") {
+						$ui.loadScript({
+							url: "https://apis.google.com/js/platform.js",
+							onload: _hangout
+						})
+					} else {
+						_hangout();
 					}
+				} catch(el) {
+					console.error(el);
 				}
-			} catch(e) {
-				console.error(e.message);
-			} finally {
-				cbk && cbk();
 			}
-		};
-	} else {
-		// Legacy UI hook
-		onload_functions.push(hangout);
-	}
+		} catch(e) {
+			console.error(e.message);
+		} finally {
+			cbk && cbk();
+		}
+	};
 
-	// Exposed functions
-	return { map: map }
+	return {
+		// Action function
+		map: function() {
+			if (typeof google === "undefined" || typeof google.maps === "undefined") {
+				$ui.loadScript({
+					url: Simplicite.GOOGLE_MAPS_JS_URL,
+					onload: _map
+				});
+			} else {
+				_map();
+			}
+		}
+	}
 })(jQuery);
--- resources/ObjectInternal/DemoOrder/SCRIPT.js
+++ resources/ObjectInternal/DemoOrder/SCRIPT.js
@@ -2,25 +2,16 @@
 // Client-side logic for order business object
 //----------------------------------------------------
 var DemoOrder = typeof DemoOrder !== "undefined" ? DemoOrder : (function($) {
-	var resp = typeof $ui !== "undefined";
-
-	// Helper to dynamically change unit price when selecting product (also done on server side)
-	if (resp) {
-		// Responsive UI hook
-		Simplicite.UI.hooks.DemoOrder = function(o, cbk) {
-			try {
-				o.locals.ui.form.onload = function(ctn, obj) {
-					var f = $ui.getUIField(ctn, obj, "demoOrdPrdId.demoPrdUnitPrice");
-					f.ui.on("change", function() {
-						$ui.getUIField(ctn, obj, "demoOrdUnitPrice").ui.val(f.ui.val());
-					});
-				};
-			} catch(e) { console.error(e.message); } finally { cbk && cbk(); }
-		};
-	} else {
-		// Legacy UI hook
-		window.demoOrdPrdId_changed = function() {
-			setFieldValue("demoOrdUnitPrice", getFieldValue("demoOrdPrdId.demoPrdUnitPrice"));
-		};
-	}
+	// Responsive UI hook
+	Simplicite.UI.hooks.DemoOrder = function(o, cbk) {
+		try {
+			// Helper to dynamically change unit price when selecting product (also done on server side)
+			o.locals.ui.form.onload = function(ctn, obj) {
+				var f = $ui.getUIField(ctn, obj, "demoOrdPrdId.demoPrdUnitPrice");
+				f.ui.on("change", function() {
+					$ui.getUIField(ctn, obj, "demoOrdUnitPrice").ui.val(f.ui.val());
+				});
+			};
+		} catch(e) { console.error(e.message); } finally { cbk && cbk(); }
+	};
 })(jQuery);
