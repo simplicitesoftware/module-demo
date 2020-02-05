ea75a66031c912a869e53b8d13dd827060bc317a
designer (Designer) <no-reply@simplicite.fr>
2020-02-05 18:22:25 +0100

    Improved mapped API example

--- src/com/simplicite/extobjects/Demo/demows.java
+++ src/com/simplicite/extobjects/Demo/demows.java
@@ -12,7 +12,7 @@ public class demows extends com.simplicite.webapp.services.RESTMappedObjectsExte
 	@Override
 	public void init(Parameters params) {
 		setOpenAPISpec(JSONTool.OPENAPI_OAS2);
-		setOpenAPIDesc("This is a **simplified** variant of the demo API for the following business objects:\n\n- Suppliers\n- Products");
+		setOpenAPIDesc("This is a **simplified** variant of the demo API for the following business objects:\n\n- Suppliers\n- Products\n- Orders");
 		setOpenAPIVers("v4.0");
 
 		addObject("suppliers", "DemoSupplier");
@@ -20,10 +20,26 @@ public class demows extends com.simplicite.webapp.services.RESTMappedObjectsExte
 		addField("suppliers", "name", "demoSupName");
 
 		addObject("products", "DemoProduct");
-		addRefField("products", "suppliers", "supplierId", "demoPrdSupId", "Reference to supplier's row ID");
+		// **Without** embedded list
+		//addRefField("products", "suppliers", "supplierId", "demoPrdSupId", "Reference to supplier's row ID");
+		// **With** embedded list
+		addRefField("products", "suppliers", "supplierId", "demoPrdSupId", "supplierProducts", true, "Reference to supplier's row ID");
 		addField("products", "supplierCode", "demoPrdSupId.demoSupCode");
 		addField("products", "supplierName", "demoPrdSupId.demoSupName");
 		addField("products", "reference", "demoPrdReference");
 		addField("products", "name", "demoPrdName");
+
+		addObject("orders", "DemoOrder");
+		// **Without** embedded list
+		//addRefField("orders", "products", "productId", "demoOrdPrdId", "Reference to product's row ID");
+		// **With** embedded list
+		addRefField("orders", "products", "productId", "demoOrdPrdId", "productOrders", true, "Reference to product's row ID");
+		addField("orders", "number", "demoOrdNumber");
+		addField("orders", "date", "demoOrdDate");
+		addField("orders", "status", "demoOrdStatus");
+		addField("orders", "productReference", "demoOrdPrdId.demoPrdReference");
+		addField("orders", "productName", "demoOrdPrdId.demoPrdName");
+		addField("orders", "productSupplierCode", "demoOrdPrdId.demoPrdSupId.demoSupCode");
+		addField("orders", "productSupplierName", "demoOrdPrdId.demoPrdSupId.demoSupName");
 	}
 }
