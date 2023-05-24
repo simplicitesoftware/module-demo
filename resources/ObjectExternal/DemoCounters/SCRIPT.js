b4c312d6348cfc225c42e4044a7080868acb0444
dazoulay@simplicite.fr (David AZOULAY) <dazoulay+dev5@simplicite.fr>
2023-05-24 19:18:29 +0200

    Counter buttons

--- resources/ObjectExternal/DemoCounters/SCRIPT.js
+++ resources/ObjectExternal/DemoCounters/SCRIPT.js
@@ -1,12 +1,17 @@
-var DemoCounters = (function() {
-	function render(params, data) {
-		$('#democounters-clients-label').text(data.clients);
-		$('#democounters-clients-value').text(data.nb_clients || 0);
-		$('#democounters-orders-label').text(data.orders);
-		$('#democounters-orders-value').text(data.nb_orders || 0);
-		$('#democounters-contacts-label').text(data.contacts);
-		$('#democounters-contacts-value').text(data.nb_contacts || 0);
-	}
-
-	return { render: render };
+var DemoCounters = (() => {
+	return {
+		render: (params, data) => {
+			$('#democounters-clients').click(() => { $ui.displayList(null, 'DemoClient'); });
+			$('#democounters-clients .label').text(data.clients);
+			$('#democounters-clients .value').text(data.nb_clients || 0);
+	
+			$('#democounters-orders').click(() => { $ui.displayList(null, 'DemoOrder'); });
+			$('#democounters-orders .label').text(data.orders);
+			$('#democounters-orders .value').text(data.nb_orders || 0);
+	
+			$('#democounters-contacts').click(() => { $ui.displayList(null, 'DemoContact'); });
+			$('#democounters-contacts .label').text(data.contacts);
+			$('#democounters-contacts .value').text(data.nb_contacts || 0);
+		}
+	};
 })();
