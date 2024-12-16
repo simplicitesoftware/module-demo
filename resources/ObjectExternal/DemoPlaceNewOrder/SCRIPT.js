23ed138007d2b27058146d33a8ad4731399255ec
designer (Designer) <>
2024-12-16 13:28:32 +0100

    Stylelint fixes

--- resources/ObjectExternal/DemoPlaceNewOrder/SCRIPT.js
+++ resources/ObjectExternal/DemoPlaceNewOrder/SCRIPT.js
@@ -9,10 +9,10 @@ var DemoPlaceNewOrder = DemoPlaceNewOrder || (function() {
 		// Override default error handler
 		const ajax = $ui.getAjax();
 		ajax.setErrorHandler(function(err) {
-			$('#DemoPlaceNewOrder-err').append($('<p/>').text(ajax.getErrorMessage(err))).show();
+			$('#demoplaceneworder-err').append($('<p/>').text(ajax.getErrorMessage(err))).show();
 		});
 
-		$('#DemoPlaceNewOrder-ord').append($ui.view.tools.panel({ title: 'Order', content: $('<div/>')
+		$('#demoplaceneworder-ord').append($ui.view.tools.panel({ title: 'Order', content: $('<div/>')
 			.append($('<div/>').append('Selected customer:'))
 			.append($('<div/>', { id: 'DemoPlaceNewOrder-selcli' }).append('&lt;none&gt;')).append('<br/>')
 			.append($('<div/>').append('Selected product:'))
@@ -42,19 +42,19 @@ var DemoPlaceNewOrder = DemoPlaceNewOrder || (function() {
 					const label = `${item.demoCliCode} - ${item.demoCliFirstname} ${item.demoCliLastname}`;
 					div.append($('<p/>', { id: `DemoPlaceNewOrder-cli_${item.row_id}` }).addClass('obj').data('item', item).click(selCli).text(label));
 				}
-				$('#DemoPlaceNewOrder-cli').append($ui.view.tools.panel({ title: 'Select customer', content: div })).slideDown();
+				$('#demoplaceneworder-cli').append($ui.view.tools.panel({ title: 'Select customer', content: div })).slideDown();
 			});
 		});
 	}
 
 	function selCli() {
 		cli.item = $(this).data('item');
-		$('#DemoPlaceNewOrder-cli').find('p').removeClass('sel');
-		$(`#DemoPlaceNewOrder-cli_${cli.item.row_id}`).addClass('sel');
-		$('#DemoPlaceNewOrder-selcli').empty().append($('<strong/>').text(`${cli.item.demoCliCode} - ${cli.item.demoCliFirstname} ${cli.item.demoCliLastname}`));
+		$('#demoplaceneworder-cli').find('p').removeClass('sel');
+		$(`#demoplaceneworder-cli_${cli.item.row_id}`).addClass('sel');
+		$('#demoplaceneworder-selcli').empty().append($('<strong/>').text(`${cli.item.demoCliCode} - ${cli.item.demoCliFirstname} ${cli.item.demoCliLastname}`));
 		if (prd?.item) {
-			$('#DemoPlaceNewOrder-ok').attr('disabled', false);
-			$('#DemoPlaceNewOrder-qty').select();
+			$('#demoplaceneworder-ok').attr('disabled', false);
+			$('#demoplaceneworder-qty').select();
 		}
 	}
 
@@ -68,16 +68,16 @@ var DemoPlaceNewOrder = DemoPlaceNewOrder || (function() {
 					const label = `${item.demoSupCode} - ${item.demoSupName}`;
 					div.append($('<p/>', { id: `DemoPlaceNewOrder-sup_${item.row_id}` }).addClass('obj').data('item', item).click(selSup).text(label));
 				}
-				$('#DemoPlaceNewOrder-sup').append($ui.view.tools.panel({ title: 'Select supplier', content: div })).slideDown();
+				$('#demoplaceneworder-sup').append($ui.view.tools.panel({ title: 'Select supplier', content: div })).slideDown();
 			});
 		});
 	}
 
 	function selSup() {
 		sup.item = $(this).data('item');
-		$('#DemoPlaceNewOrder-sup').find('p').removeClass('sel');
-		$(`#DemoPlaceNewOrder-sup_${sup.item.row_id}`).addClass('sel');
-		$('#DemoPlaceNewOrder-prd').hide().empty();
+		$('#demoplaceneworder-sup').find('p').removeClass('sel');
+		$(`#demoplaceneworder-sup_${sup.item.row_id}`).addClass('sel');
+		$('#demoplaceneworder-prd').hide().empty();
 		$ui.getUIObject('DemoProduct', 'pno_DemoProduct', function(p) {
 			prd = p;
 			prd.item = null;
@@ -89,33 +89,33 @@ var DemoPlaceNewOrder = DemoPlaceNewOrder || (function() {
 						.append($('<img/>', { src: `data:${item.demoPrdPicture.mime};base64,${item.demoPrdPicture.content}` }).css('width', '50px'))
 						.append($('<span/>').text(label)));
 				}
-				$('#DemoPlaceNewOrder-prd').append($ui.view.tools.panel({ title: 'Select product', content: div })).slideDown();
+				$('#demoplaceneworder-prd').append($ui.view.tools.panel({ title: 'Select product', content: div })).slideDown();
 			}, { demoPrdSupId: sup.item.row_id }, { inlineDocs: true });
 		});
 	}
 
 	function selPrd() {
 		prd.item = $(this).data('item');
-		$('#DemoPlaceNewOrder-prd').find('p').removeClass('sel');
-		$(`#DemoPlaceNewOrder-prd_${prd.item.row_id}`).addClass('sel');
-		$('#DemoPlaceNewOrder-selprd').empty()
+		$('#demoplaceneworder-prd').find('p').removeClass('sel');
+		$(`#demoplaceneworder-prd_${prd.item.row_id}`).addClass('sel');
+		$('#demoplaceneworder-selprd').empty()
 			.append($('<img/>', { src: `data:${prd.item.demoPrdPicture.mime};base64,${prd.item.demoPrdPicture.content}` }))
 			.append('<br/>').append($('<strong/>').text(`${prd.item.demoPrdReference} ${prd.item.demoPrdName}`));
 		if (cli?.item) {
-			$('#DemoPlaceNewOrder-ok').attr('disabled', false);
-			$('#DemoPlaceNewOrder-qty').select();
+			$('#demoplaceneworder-ok').attr('disabled', false);
+			$('#demoplaceneworder-qty').select();
 		}
 		total();
 	}
 
 	function total() {
-		$('#DemoPlaceNewOrder-err').empty();
-		const t = parseFloat(prd.item.demoPrdUnitPrice) * parseFloat($('#DemoPlaceNewOrder-qty').val());
-		$('#DemoPlaceNewOrder-total').text(t.toFixed(2));
+		$('#demoplaceneworder-err').empty();
+		const t = parseFloat(prd.item.demoPrdUnitPrice) * parseFloat($('#demoplaceneworder-qty').val());
+		$('#demoplaceneworder-total').text(t.toFixed(2));
 	}
 
 	function order() {
-		$('#DemoPlaceNewOrder-err').empty().hide();
+		$('#demoplaceneworder-err').empty().hide();
 		$ui.getUIObject('DemoOrder', 'pno_DemoOrder', function(o) {
 			ord = o;
 			ord.item = null;
@@ -125,9 +125,9 @@ var DemoPlaceNewOrder = DemoPlaceNewOrder || (function() {
 				ord.item.demoOrdPrdId = prd.item.row_id;
 				// ZZZ populate must be called to set all referred fields from client and product before creation
 				ord.populate(function() {
-					ord.item.demoOrdQuantity = $('#DemoPlaceNewOrder-qty').val();
+					ord.item.demoOrdQuantity = $('#demoplaceneworder-qty').val();
 					ord.create(function() {
-						$('#DemoPlaceNewOrder').html(`<p>Order created with number ${ord.item.demoOrdNumber}<br/>Thank you !</p>`);
+						$('#demoplaceneworder').html(`<p>Order created with number ${ord.item.demoOrdNumber}<br/>Thank you !</p>`);
 						$ui.view.notify({ type: 'create', object: ord, rowId: ord.item.row_id }); // Notify UI components (e.g. menu)
 					});
 				});
