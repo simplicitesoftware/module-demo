6609758581b05d6a0f9497fcb120f0c31b12ff8a
designer (Designer) <>
2025-02-18 18:09:32 +0100

    Improved client side logic

--- resources/ObjectInternal/DemoOrder/CLASS.js
+++ resources/ObjectInternal/DemoOrder/CLASS.js
@@ -6,8 +6,26 @@ Simplicite.UI.BusinessObjects.DemoOrder = class extends Simplicite.UI.BusinessOb
 	onLoadForm(ctn, obj, p) {
 		super.onLoadForm(ctn, obj, p);
 
-		// Helper to dynamically change unit price when selecting product (also done on server side)
-		var up = $ui.getUIField(ctn, obj, 'demoOrdPrdId.demoPrdUnitPrice').ui;
-		up.on('change', () => $ui.getUIField(ctn, obj, 'demoOrdUnitPrice').ui.val(up.val()));
+		// Dynamic client-side logic (overriden anyway by server-side logic at save)
+
+		const pup = $ui.getUIField(ctn, obj, 'demoOrdPrdId.demoPrdUnitPrice').ui;
+		const up = $ui.getUIField(ctn, obj, 'demoOrdUnitPrice').ui;
+		const q = $ui.getUIField(ctn, obj, 'demoOrdQuantity').ui;
+		const t = $ui.getUIField(ctn, obj, 'demoOrdTotal').ui;
+		const v = $ui.getUIField(ctn, obj, 'demoOrdVAT').ui;
+
+		const calcTotal = () => {
+			t.val(q.val() * up.val());
+			v.val(t.val() * parseFloat($grant.sysparams.DEMO_VAT) / 100);
+		};
+
+		// Change unit price if product is changed
+		pup.on('change', () => {
+			up.val(pup.val());
+			calcTotal();
+		});
+
+		// recalculate total if quantity changes
+		q.on('change', calcTotal);
 	}
 };
