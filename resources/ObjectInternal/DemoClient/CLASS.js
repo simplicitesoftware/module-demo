0dc7904ca7806d2bd5b917a725d20c4aa7ad7253
designer (Designer) <>
2025-04-25 12:19:24 +0200

    Default themes + JS syntax

--- resources/ObjectInternal/DemoClient/CLASS.js
+++ resources/ObjectInternal/DemoClient/CLASS.js
@@ -2,32 +2,30 @@
 // Client-side logic for order business object
 //----------------------------------------------------
 
-/* global*/
-
 Simplicite.UI.BusinessObjects.DemoClient = class extends Simplicite.UI.BusinessObject {
 	// Action function
 	map() {
 		// A Promise to load the Leaflet library
-		const loadLeafLet = new Promise((resolve,reject)=>{
+		const loadLeafLet = new Promise((resolve, reject) => {
 			$ui.loadParts([
 				{
 					name:'leaflet-css',
-					url: $root+'/scripts/leaflet/leaflet.css',
+					url: `${$root}/scripts/leaflet/leaflet.css`,
 					type: 'CSS'
 				},
 				{
 					name:'leaflet-js',
-					url: $root+'/scripts/leaflet/leaflet.js',
+					url: `${$root}/scripts/leaflet/leaflet.js`,
 				}
 			], resolve);
 		});
 		
 		// A Promise to get the client's coordinates and data in a convenient format
-		const loadData = new Promise((resolve,reject)=>{
+		const loadData = new Promise((resolve, reject) => {
 			let data = {};
 			data.row_id = $ui.getUIField(null, this, 'row_id').ui.val();
 			if (data.row_id && data.row_id != '0') {
-				const coords = $ui.getUIField(null, this, 'demoCliCoords').ui.val().replace(';',',').split(',');
+				const coords = $ui.getUIField(null, this, 'demoCliCoords').ui.val().replace(';', ',').split(',');
 				data.latitude = coords[0];
 				data.longitude = coords[1];
 				data.name = $.escapeHTML(
@@ -48,18 +46,18 @@ Simplicite.UI.BusinessObjects.DemoClient = class extends Simplicite.UI.BusinessO
 		});
 		
 		// A Promise to get the Map settings
-		const getSettings = new Promise(res=>$ui.grant.getParameter(s=>res(JSON.parse(s)),'MAP_SETTINGS'));
+		const getSettings = new Promise(res => $ui.grant.getParameter(s => res(JSON.parse(s)), 'MAP_SETTINGS'));
 		
 		Promise
-			.all([loadData,getSettings,loadLeafLet])
-			.then(([data,mapSettings])=>{
+			.all([loadData, getSettings, loadLeafLet])
+			.then(([data, mapSettings])=>{
 				// Show the container where the map is displayed
 				$('#client-map').show();
 				
 				// Add a map to that container, centered on the client's coords 
 				var map = L.map('client-map').setView([data.latitude,data.longitude], 13);
 				
-				// Hide the attribution prefix ("Leaflet" branding on the map)
+				// Hide the attribution prefix (Leaflet branding on the map)
 				map.attributionControl.setPrefix(false);
 				
 				// Configure the map with the tile layer defined in the settings
@@ -69,10 +67,10 @@ Simplicite.UI.BusinessObjects.DemoClient = class extends Simplicite.UI.BusinessO
 				}).addTo(map);
 				
 				// Add a marker on the map
-				var marker = L.marker([data.latitude,data.longitude]).addTo(map);
+				var marker = L.marker([data.latitude, data.longitude]).addTo(map);
 				
 				// Show popup with extra info on marker click
-				marker.bindPopup("<b>"+data.name+"</b><br>"+data.address);
+				marker.bindPopup(`<b>${data.name}</b><br>${data.address}`);
 			});
 	}
 };
