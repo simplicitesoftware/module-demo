80431e251cb8b38ef5ed50d15bf820c7131a9a08
David AZOULAY <dazoulay@simplicite.fr>
2025-04-25 12:30:12 +0200

    ESLint fixes

--- resources/ObjectInternal/DemoClient/CLASS.js
+++ resources/ObjectInternal/DemoClient/CLASS.js
@@ -2,11 +2,13 @@
 // Client-side logic for order business object
 //----------------------------------------------------
 
+/* global L */
+
 Simplicite.UI.BusinessObjects.DemoClient = class extends Simplicite.UI.BusinessObject {
 	// Action function
 	map() {
 		// A Promise to load the Leaflet library
-		const loadLeafLet = new Promise((resolve, reject) => {
+		const loadLeafLet = new Promise((resolve, _reject) => {
 			$ui.loadParts([
 				{
 					name:'leaflet-css',
@@ -19,7 +21,7 @@ Simplicite.UI.BusinessObjects.DemoClient = class extends Simplicite.UI.BusinessO
 				}
 			], resolve);
 		});
-		
+
 		// A Promise to get the client's coordinates and data in a convenient format
 		const loadData = new Promise((resolve, reject) => {
 			let data = {};
@@ -44,31 +46,31 @@ Simplicite.UI.BusinessObjects.DemoClient = class extends Simplicite.UI.BusinessO
 				reject();
 			}
 		});
-		
+
 		// A Promise to get the Map settings
 		const getSettings = new Promise(res => $ui.grant.getParameter(s => res(JSON.parse(s)), 'MAP_SETTINGS'));
-		
+
 		Promise
 			.all([loadData, getSettings, loadLeafLet])
 			.then(([data, mapSettings])=>{
 				// Show the container where the map is displayed
 				$('#client-map').show();
-				
-				// Add a map to that container, centered on the client's coords 
+
+				// Add a map to that container, centered on the client's coords
 				var map = L.map('client-map').setView([data.latitude,data.longitude], 13);
-				
+
 				// Hide the attribution prefix (Leaflet branding on the map)
 				map.attributionControl.setPrefix(false);
-				
+
 				// Configure the map with the tile layer defined in the settings
 				L.tileLayer(mapSettings.tileLayer, {
 				    maxZoom: mapSettings.maxZoom,
 				    attribution: mapSettings.attribution
 				}).addTo(map);
-				
+
 				// Add a marker on the map
 				var marker = L.marker([data.latitude, data.longitude]).addTo(map);
-				
+
 				// Show popup with extra info on marker click
 				marker.bindPopup(`<b>${data.name}</b><br>${data.address}`);
 			});
